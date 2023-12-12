770f4f392725905c2598d7a030b4de5bea52fda3
zeinab313 <152908582+zeinab313@users.noreply.github.com>
2023-12-12 12:24:12 -0800

    created count view
    In order to add a number to the number of views with each visit of the desired post

--- blog/views.py
+++ blog/views.py
@@ -9,5 +9,8 @@ def blog_view(request):
 
 def blog_single(request,pid):
     post=get_object_or_404(Post,pk=pid)
+    #برای اینکه با هر بار بازدید پست موردنظر یک بازدید بخورد
+    post.counted_views=post.counted_views+1
+    post.save()
     contex={'post':post}
     return render(request,'blog/blog-single.html',contex)
