8010a9a78bdb6f4a7d2b076430b7cd43b0cb0b33
zeinab313 <152908582+zeinab313@users.noreply.github.com>
2023-12-12 19:01:21 -0800

    the first part practice the sixth chapter
    the first part practice the sixth chapter for published view

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
+from django.utils import timezone
 
 # Create your views here.
 def blog_view(request):
-    posts=Post.objects.filter(status=1)
+    # posts=Post.objects.filter(status=1)
+    #مقایسه زمان انتشار با زمان گذشته
+    posts = Post.objects.filter(published_date__lte=timezone.now(), status=1)
+    print(timezone.now())
     context={'posts':posts}
     return render(request,'blog/blog-home.html',context)
 
 def blog_single(request,pid):
     post=get_object_or_404(Post,pk=pid)
-    #برای اینکه با هر بار بازدید پست موردنظر یک بازدید بخورد
+    #برای اینکه با هر بار بازدید پست موردنظر یک عدد به تعداد بازدید اضافه شود
     post.counted_views=post.counted_views+1
     post.save()
     contex={'post':post}
