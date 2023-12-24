4178b2982c8359f80ca56719824ad07fd888502b
zeinab313 <152908582+zeinab313@users.noreply.github.com>
2023-12-24 12:15:15 -0800

    create app accounts
    create app accounts and pages login.html and signup.html

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name="accounts"
+
+urlpatterns = [
+    #login
+    path('login',views.login_view,name='login'),
+    #logout
+    # path('logout',views.logout_view,name='logout'),
+    #registrations/signup
+    path('signup',views.signup_view,name='signup'),
+]
