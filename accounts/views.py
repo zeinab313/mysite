96a173976608c414c4d481a9e7e84a248ec6593f
zeinab313 <152908582+zeinab313@users.noreply.github.com>
2023-12-25 10:08:59 -0800

    create page sign up
    create page sign up

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 
 from django.shortcuts import redirect, render
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -22,10 +23,21 @@ def login_view(request):
     else:
                    return redirect('/')
 
+@login_required
 def logout_view(request):
-     if request.user.is_authenticated:
+    #  if request.user.is_authenticated:
           logout(request)
           return redirect('/')
 
 def signup_view(request):
-    return render(request,'accounts/signup.html')
+    if not request.user.is_authenticated:
+        if request.method=="POST":
+              form=UserCreationForm(request.POST)
+              if form.is_valid():
+                form.save()
+                return redirect('/')
+        form=UserCreationForm()
+        context={'form':form}
+        return render(request,'accounts/signup.html',context)
+    else:
+          return redirect('/')
